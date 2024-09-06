e2d9662779047173001b529e094e386435191c0d
ayana-uts <ayana.fukui@student.uts.edu.au>
2024-09-06 17:14:29 +1000

    80 percent

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,21 +8,58 @@ public class InputManager : MonoBehaviour
 
     void Start()
     {
-        // サイズ2のTransform配列を宣言
+        // サイズ2のTransform配列を初期化
         transArray = new Transform[2];
 
-        // redPrefabとbluePrefabのTransformを取得して配列に割り当て
-        GameObject redPrefab = GameObject.FindWithTag("Red");
-        GameObject bluePrefab = GameObject.FindWithTag("Blue");
-
-        if (redPrefab != null)
+        // GameObjectのタグを使用してPrefabのTransformを検索し、配列に割り当て
+        GameObject redObj = GameObject.FindWithTag("Red");
+        if (redObj != null)
         {
-            transArray[0] = redPrefab.transform;
+            transArray[0] = redObj.transform;
+        }
+        else
+        {
+            Debug.LogError("No GameObject with the tag 'Red' was found in the scene.");
         }
 
-        if (bluePrefab != null)
+        GameObject blueObj = GameObject.FindWithTag("Blue");
+        if (blueObj != null)
         {
-            transArray[1] = bluePrefab.transform;
+            transArray[1] = blueObj.transform;
+        }
+        else
+        {
+            Debug.LogError("No GameObject with the tag 'Blue' was found in the scene.");
+        }
+    }
+
+    void Update()
+    {
+        // "W"キーが押された場合の処理
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // z軸を中心にredObjを45度回転
+            // RotateAroundを使用してredObjを45度回転
+                transArray[0].Rotate(Vector3.forward,angle:45f);
+            
+
+            // z軸を中心にblueObjを-45度回転
+            
+                transArray[1].Rotate(Vector3.forward, angle: -45f);
+           
+        }
+
+        // "Q"キーが押された場合の処理
+        if (Input.GetButtonDown("Fire1")) // "Fire1"はQボタンにマッピングされている必要があります
+        {
+            
+                // redObjとblueObjの垂直位置を交換
+                float temp = transArray[0].position.y;
+            
+            transArray[0].position = new Vector3(transArray[0].position.x, transArray[1].position.y, transArray[0].position.z);
+            transArray[1].position = new Vector3(transArray[1].position.x, y:temp, transArray[1].position.z);
+
+
         }
     }
 }
