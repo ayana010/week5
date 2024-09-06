2676ece5bb7461ec21092e4f756ef68821685b55
ayana-uts <ayana.fukui@student.uts.edu.au>
2024-09-06 16:52:42 +1000

    60 percent

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private Transform[] transArray;
+
+    void Start()
+    {
+        // サイズ2のTransform配列を宣言
+        transArray = new Transform[2];
+
+        // redPrefabとbluePrefabのTransformを取得して配列に割り当て
+        GameObject redPrefab = GameObject.FindWithTag("Red");
+        GameObject bluePrefab = GameObject.FindWithTag("Blue");
+
+        if (redPrefab != null)
+        {
+            transArray[0] = redPrefab.transform;
+        }
+
+        if (bluePrefab != null)
+        {
+            transArray[1] = bluePrefab.transform;
+        }
+    }
+}
+
--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadAssets : MonoBehaviour {
+public class LoadAssets : MonoBehaviour
+{
+    public GameObject redPrefab;  // redPrefabの参照
+    public GameObject bluePrefab; // bluePrefabの参照
 
-    public GameObject redObj;
-    [SerializeField]
-    private GameObject blueObj;
+    private GameObject redObj;    // redObjの参照
+    private GameObject blueObj;   // blueObjの参照
 
-	// Use this for initialization
-	void Start () {
-        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
-        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
+    void Awake()
+    {
+        // redPrefabとbluePrefabをインスタンス化してシーンに配置
+        redObj = Instantiate(redPrefab);
+        blueObj = Instantiate(bluePrefab);
 
+        // redObjとblueObjをそれぞれ指定された位置に配置
+        redObj.transform.position = new Vector3(2, 1, 0);
+        blueObj.transform.position = new Vector3(-2, -1, 0);
+
+        // タグを設定する（もしプレハブにタグが設定されていない場合）
+        redObj.tag = "Red";
+        blueObj.tag = "Blue";
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
+
